763b04822866f56fa1bcd78316d49f62aa44046a
Julio_Mesquita <juliomartins@lampp-it.com.br>
2024-10-13 17:30:56 -0300

    Congigurado os beans de caso de uso na camada infrastructure

---
+++ infrastructure/src/main/java/com/juliomesquita/admin/catalog/infrastructure/configuration/category/CategoryUseCaseConfig.java
@@ -0,0 +1,52 @@
+package com.juliomesquita.admin.catalog.infrastructure.configuration.category;
+
+import com.juliomesquita.admin.catalog.application.usecases.category.create.CreateCategoryUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.create.DefaultCreateCategoryUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.delete.DefaultDeleteCategoryUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.delete.DeleteCategoryUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.retrive.getbyid.DefaultGetCategoryByIdUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.retrive.getbyid.GetCategoryByIdUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.retrive.list.DefaultListCategoriesUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.retrive.list.ListCategoriesUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.update.DefaultUpdateCategoryUseCase;
+import com.juliomesquita.admin.catalog.application.usecases.category.update.UpdateCategoryUseCase;
+import com.juliomesquita.admin.catalog.domain.category.CategoryGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class CategoryUseCaseConfig {
+
+    private final CategoryGateway categoryGateway;
+
+    public CategoryUseCaseConfig(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Bean
+    public CreateCategoryUseCase createCategoryUseCase() {
+        return new DefaultCreateCategoryUseCase(categoryGateway);
+    }
+
+    @Bean
+    public GetCategoryByIdUseCase getCategoryByIdUseCase() {
+        return new DefaultGetCategoryByIdUseCase(categoryGateway);
+    }
+
+    @Bean
+    public ListCategoriesUseCase listCategoriesUseCase() {
+        return new DefaultListCategoriesUseCase(categoryGateway);
+    }
+
+    @Bean
+    public UpdateCategoryUseCase updateCategoryUseCase() {
+        return new DefaultUpdateCategoryUseCase(categoryGateway);
+    }
+
+    @Bean
+    public DeleteCategoryUseCase deleteCategoryUseCase() {
+        return new DefaultDeleteCategoryUseCase(categoryGateway);
+    }
+}
